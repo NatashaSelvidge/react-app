8b1f9767b73741cf04f071b7c3da6e7e3d780a16
Natasha A Selvidge <natasha.selvidge@gmail.com>
2020-09-18 12:45:06 -0700

    Change the loader color to aqua

--- src/Weather.js
+++ src/Weather.js
@@ -19,7 +19,7 @@ export default function Weather(props) {
   return (
     <Loader
       type="Puff"
-      color="black"
+      color="aqua"
       height={100}
       width={100}
       timeout={3000} //3 secs

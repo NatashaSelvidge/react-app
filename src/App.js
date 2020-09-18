704dac98006a8c2cbbe9c6b35337787b4d7f2c31
Natasha A Selvidge <natasha.selvidge@gmail.com>
2020-09-18 10:39:24 -0700

    added App from to the import App.css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
-
-import "./App.css";
+//import "./App.css";
 import Weather from "./Weather";
 
 function App() {
--- src/Weather.js
+++ src/Weather.js
@@ -19,7 +19,7 @@ export default function Weather(props) {
   return (
     <Loader
       type="Puff"
-      color="aqua"
+      color="black"
       height={100}
       width={100}
       timeout={3000} //3 secs
